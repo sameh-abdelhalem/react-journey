b68cdaa02aa42bfe8a224f29839f6707b4aa27a2
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2022-02-22 22:56:02 +0200

    set Expenses dynamicly via states

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -12,21 +12,8 @@ const Expenses = (props) => {
     <div>
     <Card className="expenses">
       <ExpensesFilter selectedDate={filteredDate} onDateSelection={onDateSelectionHandler}/>
-      <ExpenseItem
-        date={props.expenses[0].date}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={props.expenses[1].date}
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={props.expenses[2].date}
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-      ></ExpenseItem>
+      {props.expenses.map(expense=> <ExpenseItem date={expense.date} title = {expense.title} amount={expense.amount} />)}
+    
     </Card>
     </div>
   );
