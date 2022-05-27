097ac7e3e0ffcd19d9b19dda7c99275754021580
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2022-05-27 19:38:21 +0200

    adjust bar fill

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,20 +4,25 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
-  const [filteredDate,setFilteredDate] = useState('2019')
-  const onDateSelectionHandler=(selectedDate)=>{
-
-    setFilteredDate(selectedDate.toString())
-  }
-const filteredExpenses = props.expenses.filter((expense)=>expense.date.getFullYear().toString()===filteredDate)
+  const [filteredDate, setFilteredDate] = useState("2019");
+  const onDateSelectionHandler = (selectedDate) => {
+    setFilteredDate(selectedDate.toString());
+  };
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredDate
+  );
   return (
     <div>
-    <Card className="expenses">
-      <ExpensesFilter selectedDate={filteredDate} onDateSelection={onDateSelectionHandler}/>
-      <ExpensesList items={filteredExpenses}/>  
-    
-    </Card> 
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedDate={filteredDate}
+          onDateSelection={onDateSelectionHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
     </div>
   );
 };
